4745cba7190243ac014aaf66073a591b9b064d30
“João <eng.joao.pedro@outlook.com>
2022-12-29 18:59:02 -0300

    refactor: add alert

--- src/App.js
+++ src/App.js
@@ -12,13 +12,30 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+
+    if (!name) {
+      /* if is empity display alert */
+      /* setAlert({ show: true, msg: "please enter value", type: "danger" }); */
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
+      /* deal with edit */
+    } else {
+      /* create object */
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      /* get previous values from list and add one item */
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
   };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -33,10 +50,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
