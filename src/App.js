864fbd4251606b01f01377279008958a5daf8dfe
“João <eng.joao.pedro@outlook.com>
2022-12-29 19:05:59 -0300

    refactor: message create object and clear list

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
       /* deal with edit */
     } else {
       /* create object */
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       /* get previous values from list and add one item */
       setList([...list, newItem]);
@@ -32,10 +33,15 @@ function App() {
     setAlert({ show, type, msg });
   };
 
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -53,7 +59,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
